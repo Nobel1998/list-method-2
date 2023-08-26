187c5e55b4a90512acd905484cb2e79449b8cce3
Nobel1998 <104122928+Nobel1998@users.noreply.github.com>
2023-08-26 11:48:43 -0700

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -1,1 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+interface StringChecker { boolean checkString(String s); }
+
+class ListExamples {
+
+  // Returns a new list that has all the elements of the input list for which
+  // the StringChecker returns true, and not the elements that return false, in
+  // the same order they appeared in the input list;
+  static List<String> filter(List<String> list, StringChecker sc) {
+    List<String> result = new ArrayList<>();
+    for(String s: list) {
+      if(sc.checkString(s)) {
+        result.add(s);
+      }
+    }
+    return result;
+  }
+
+   static List<String> merge(List<String> list1, List<String> list2) {
+    throw new RuntimeException("Unexpected error!");
+}
+}
+
 
